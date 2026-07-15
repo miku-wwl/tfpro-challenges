f0cbe3e58861c372bb4e7eeca5183ddcd156827f
miku-wwl <569144003@qq.com>
2026-07-15 16:01:48 +1200

    Complete challenge 6 task 4 provider aliases

--- challenge-6/challenge-6.tf
+++ challenge-6/challenge-6.tf
@@ -8,6 +8,67 @@ terraform {
 }
 
 provider "aws" {
+  region                   = "us-east-1"
+  alias                    = "iam_access"
+  shared_config_files      = ["${path.module}/.aws/config"]
+  shared_credentials_files = ["${path.module}/.aws/credentials"]
+  profile                  = "iam-access"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  alias                    = "ec2_access"
+  shared_config_files      = ["${path.module}/.aws/config"]
+  shared_credentials_files = ["${path.module}/.aws/credentials"]
+  profile                  = "ec2-access"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  alias                    = "readonly_access"
+  shared_config_files      = ["${path.module}/.aws/config"]
+  shared_credentials_files = ["${path.module}/base-folder/default-creds.txt"]
+  profile                  = "readonly-access"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
+}
+
+
+provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
@@ -26,10 +87,13 @@ provider "aws" {
 
 resource "aws_security_group" "allow_tls" {
   name = "demo-firewall"
+  provider = aws.ec2_access
 
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  provider = aws.readonly_access
+}
 
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
@@ -39,6 +103,7 @@ output "account_id" {
 resource "aws_iam_role" "cw_full_access" {
   name                = "CloudWatchFullAccess"
   managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchFullAccess"]
+  provider = aws.iam_access
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
