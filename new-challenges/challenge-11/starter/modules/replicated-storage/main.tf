d842b9c4462d9710302a6d1112ea27754ba483d8
miku-wwl <569144003@qq.com>
2026-07-16 14:36:15 +1200

    challenge11 完成

--- new-challenges/challenge-11/starter/modules/replicated-storage/main.tf
+++ new-challenges/challenge-11/starter/modules/replicated-storage/main.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "primary" {
 }
 
 data "aws_caller_identity" "recovery" {
-  # TODO 3: bind the recovery caller lookup to aws.recovery.
+  provider = aws.recovery
 }
 
 resource "aws_s3_bucket" "primary" {
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "primary" {
 }
 
 resource "aws_s3_bucket" "recovery" {
-  # TODO 4: bind the recovery bucket to aws.recovery.
+  provider      = aws.recovery
   bucket        = "${var.name_prefix}-recovery"
   force_destroy = true
   tags          = merge(var.common_tags, { role = "recovery", region = var.recovery_region })
