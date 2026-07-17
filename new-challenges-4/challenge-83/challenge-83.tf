cfa2fb4dced25c7362063a32ec0ac401ce214e67
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-18 01:20:39 +1200

    Add challenge 83 random key failure example

--- new-challenges-4/challenge-83/challenge-83.tf
+++ new-challenges-4/challenge-83/challenge-83.tf
@@ -58,5 +58,21 @@ output "starter_bucket" {
   value = aws_s3_bucket.inventory.id
 }
 
-# Tasks 2-5 add random shards, reproduce two invalid for_each expressions,
-# and publish manifests under stable public service keys.
+resource "random_integer" "shard" {
+  for_each = var.service_names
+  min      = 100
+  max      = 999
+  keepers = {
+    name = each.key
+  }
+}
+
+resource "aws_s3_object" "invalid_random_key" {
+  for_each = toset([
+    for name in var.service_names : random_integer.shard[name].result
+  ])
+
+  bucket = aws_s3_bucket.inventory.id
+  key    = each.key
+
+}
